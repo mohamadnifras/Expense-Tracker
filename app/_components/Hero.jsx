0bed3a0a2f731abd78f08bed82964724d2002ce3
mohamadnifras <mohamadnifras2004@gmail.com>
2025-12-05 00:03:04 +0530

    complited project

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Hero() {
   return (
     <section className="bg-gray-50 lg:h-screen lg:place-content-center flex items-center flex-col">
-  <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+  <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32 mt-10">
     <div className="mx-auto max-w-prose text-center">
       <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
         Manager Your Expense
